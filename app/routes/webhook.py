63682f20a66614fa69b34b2f648291ffdcca7fdd
FRNS1 <fernandesjoao2009@hotmail.com>
2023-12-06 15:55:38 -0300

    up

--- app/routes/webhook.py
+++ app/routes/webhook.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify, make_response
 
 webhook_bp = Blueprint('webhook', __name__)
 
-@webhook_bp.route('/getinvoice', methods=['GET'])
+@webhook_bp.route('/getinvoice', methods=['POST'])
 def hookReceiver():
     return make_response(
         jsonify(
