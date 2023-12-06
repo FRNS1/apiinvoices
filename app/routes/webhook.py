31e5e15d7a68c12838237e386f777c9a1ca27bd7
FRNS1 <fernandesjoao2009@hotmail.com>
2023-12-06 17:05:36 -0300

    up

--- app/routes/webhook.py
+++ app/routes/webhook.py
@@ -8,7 +8,7 @@ def hookReceiver():
     data = request.get_json()
     if data:
         print('------------------------------------')
-        print(data)
+        print(data['event'])
         print('------------------------------------')
         amount = data['event']['invoice']['amount']
         status = data['event']['invoice']['status']
