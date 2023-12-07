63b4a155b21c12ec7d8824bfc6026e9851d18a4e
FRNS1 <fernandesjoao2009@hotmail.com>
2023-12-07 11:06:42 -0300

    up

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,25 @@
 from flask import Flask
 from flask_cors import CORS
-from .routes.invoices import invoices_bp
-from .routes.webhook import webhook_bp
+from app.routes.invoices import invoices_bp
+from app.routes.webhook import webhook_bp
 from .routes.services.functionsFile import functions
 from flask_apscheduler import APScheduler
-from datetime import datetime, timedelta
-
-
 
 def create_app():
     app = Flask(__name__)
     scheduler = APScheduler()
     CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+    
+    ### Função para ser usada no scheduler
+    def sendInvoices():
+        invoices = functions.sendInvoicesEvery3Hours()
         
     ### Faz o agendamento do Job para enviar invoices a cada 3 horas
     app.config['SCHEDULER_API_ENABLED'] = True
     app.config['JOBS'] = [
         {
             'id': 'create_invoice_job',
-            'func': functions.sendInvoicesEvery3Hours(),
+            'func': sendInvoices,
             'trigger': 'interval',
             'hours': 1,
         }
